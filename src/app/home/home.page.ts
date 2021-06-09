60b4b14cffbfa022cd8cb0e76d1f305305e91f77
ahmer-najam <53119340+ahmer-najam@users.noreply.github.com>
2021-06-09 11:09:42 +0500

    2021.06.09.001

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit, OnDestroy {
+  constructor(private alertController: AlertController) {}
 
-  constructor() {}
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+  }
 
+  ngOnDestroy() {}
+
+  stopScan = () => {
+    BarcodeScanner.showBackground();
+    BarcodeScanner.stopScan();
+  };
+
+  prepare = () => {
+    BarcodeScanner.prepare();
+  };
+
+  startScan = async () => {
+    BarcodeScanner.hideBackground();
+    const result = await BarcodeScanner.startScan();
+    if (result.hasContent) {
+      console.log(result.content);
+    }
+  };
+
+  async startScanner() {}
 }
